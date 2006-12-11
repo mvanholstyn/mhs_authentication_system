d8dca7170c438745d64499dcc971a86aa5c9a8d6
Mark Van Holstyn <mvanholstyn@gmail.com>
2006-12-11 14:30:01 +0000

    Added GroupPrivilege model

--- init.rb
+++ init.rb
@@ -1,8 +1,9 @@
 dir = File.join( File.dirname( __FILE__ ), 'lib' )
 
-require_dependency File.join( dir, 'lwt_authentication_system' )
-require_dependency File.join( dir, 'group' )
-require_dependency File.join( dir, 'privilege' )
+require( 'lwt_authentication_system' )
+require( 'group' )
+require( 'privilege' )
+require( 'group_privilege' )
 #require_dependency 'preference'
 
 ActiveRecord::Base.extend LWT::AuthenticationSystem::Model::SingletonMethods
