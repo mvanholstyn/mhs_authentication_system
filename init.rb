024f6a817be9259490e6b6f8e6c9115e6c312fb2
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-04-16 16:07:02 +0000

    Nevermind...

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
 dir = File.join( File.dirname( __FILE__ ), 'lib' )
-$:.unshift dir
 
 require File.join( dir, 'model' )
 require File.join( dir, 'controller' )
 require File.join( dir, 'login_controller' )
+require File.join( dir, 'group_model' )
+require File.join( dir, 'privilege_model' )
+require File.join( dir, 'group_privilege_model' )
 ActiveRecord::Base.send :include, LWT::AuthenticationSystem::Model
 ActionController::Base.send :include, LWT::AuthenticationSystem::Controller
 ActionController::Base.send :include, LWT::AuthenticationSystem::LoginController
