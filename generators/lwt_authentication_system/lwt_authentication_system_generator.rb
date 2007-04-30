b7284c954e24e1fe9d14198af8f93363daa706b5
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-04-30 17:53:16 +0000

    Updated class collisions check

--- generators/lwt_authentication_system/lwt_authentication_system_generator.rb
+++ generators/lwt_authentication_system/lwt_authentication_system_generator.rb
@@ -5,22 +5,25 @@ class LwtAuthenticationSystemGenerator < Rails::Generator::Base
   #TODO: Add tests
   def manifest
     record do |m|
-      m.class_collisions *%w{ Privilege Group GroupPrivilege User UsersController }
+      #m.class_collisions *%w{ UsersController Privilege Group GroupPrivilege User ForgotPassword ForgotPasswordMailer }
 
-      m.directory File.join( *%w{ app models } )
       m.directory File.join( *%w{ app controllers } )
-      m.directory File.join( *%w{ app views users } )
+      m.directory File.join( *%w{ app models } )
       m.directory File.join( *%w{ app views forgot_password_mailer } )
-      m.directory File.join( *%w{ db migrate } )
+      m.directory File.join( *%w{ app views users } )
       m.directory File.join( *%w{ test fixtures } )
+      m.directory File.join( *%w{ db migrate } )
 
-      m.template 'model.rb', File.join( *%w{ app models user.rb } )
-      m.template 'login_controller.rb', File.join( *%w{ app controllers users_controller.rb } )
-      m.template 'login.erb', File.join( *%W{ app views users login.erb } )
-      m.template 'privileges.yml', File.join( *%w{ test fixtures privileges.yml } )
-      m.template 'groups.yml', File.join( *%w{ test fixtures groups.yml } )
-      m.template 'groups_privileges.yml', File.join( *%w{ test fixtures groups_privileges.yml } )
-      m.template 'users.yml', File.join( *%w{ test fixtures users.yml } )
+      m.template 'app/controllers/login_controller.rb', File.join( *%w{ app controllers users_controller.rb } )
+      m.template 'app/models/model.rb', File.join( *%w{ app models user.rb } )
+      m.template 'app/views/forgot_password_mailer/forgot_password.erb', File.join( *%W{ app views forgot_password_mailer forgot_password.erb } )
+      m.template 'app/views/users/change_password.erb', File.join( *%W{ app views users change_password.erb } )
+      m.template 'app/views/users/forgot_password.erb', File.join( *%W{ app views users forgot_password.erb } )
+      m.template 'app/views/users/login.erb', File.join( *%W{ app views users login.erb } )
+      m.template 'test/fixtures/groups.yml', File.join( *%w{ test fixtures groups.yml } )
+      m.template 'test/fixtures/groups_privileges.yml', File.join( *%w{ test fixtures groups_privileges.yml } )
+      m.template 'test/fixtures/privileges.yml', File.join( *%w{ test fixtures privileges.yml } )
+      m.template 'test/fixtures/users.yml', File.join( *%w{ test fixtures users.yml } )
       m.migration_template 'migration.rb', File.join( *%w{ db migrate } ), :migration_file_name => "add_lwt_authentication_system"
     end
   end
