2bd7e781a94a14cb06951bf959a72b17dc6b509f
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-07-19 04:02:15 +0000

    Updated generator to generate fixtures for specs. Update generator to generate with signup on

--- generators/lwt_authentication_system/lwt_authentication_system_generator.rb
+++ generators/lwt_authentication_system/lwt_authentication_system_generator.rb
@@ -5,13 +5,11 @@ class LwtAuthenticationSystemGenerator < Rails::Generator::Base
   #TODO: Add tests
   def manifest
     record do |m|
-      #m.class_collisions *%w{ UsersController Privilege Group GroupPrivilege User ForgotPassword ForgotPasswordMailer }
-
       m.directory File.join( *%w{ app controllers } )
       m.directory File.join( *%w{ app models } )
       m.directory File.join( *%w{ app views user_reminder_mailer } )
       m.directory File.join( *%w{ app views users } )
-      m.directory File.join( *%w{ test fixtures } )
+      m.directory File.join( *%w{ spec fixtures } )
       m.directory File.join( *%w{ db migrate } )
 
       m.template 'app/controllers/login_controller.rb', File.join( *%w{ app controllers users_controller.rb } )
@@ -21,13 +19,11 @@ class LwtAuthenticationSystemGenerator < Rails::Generator::Base
       m.template 'app/views/users/profile.html.erb', File.join( *%W{ app views users profile.html.erb } )
       m.template 'app/views/users/reminder.html.erb', File.join( *%W{ app views users reminder.html.erb } )
       m.template 'app/views/users/signup.html.erb', File.join( *%W{ app views users signup.html.erb } )
-      m.template 'test/fixtures/groups.yml', File.join( *%w{ test fixtures groups.yml } )
-      m.template 'test/fixtures/groups_privileges.yml', File.join( *%w{ test fixtures groups_privileges.yml } )
-      m.template 'test/fixtures/privileges.yml', File.join( *%w{ test fixtures privileges.yml } )
-      m.template 'test/fixtures/users.yml', File.join( *%w{ test fixtures users.yml } )
+      m.template 'spec/fixtures/groups.yml', File.join( *%w{ spec fixtures groups.yml } )
+      m.template 'spec/fixtures/groups_privileges.yml', File.join( *%w{ spec fixtures groups_privileges.yml } )
+      m.template 'spec/fixtures/privileges.yml', File.join( *%w{ spec fixtures privileges.yml } )
+      m.template 'spec/fixtures/users.yml', File.join( *%w{ spec fixtures users.yml } )
       m.migration_template 'db/migrate/migration.rb', File.join( *%w{ db migrate } ), :migration_file_name => "add_lwt_authentication_system"
-      
-      # m.route 'config/routes.rb'
     end
   end
 end
