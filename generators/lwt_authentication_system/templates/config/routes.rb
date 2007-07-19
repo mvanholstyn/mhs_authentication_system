25ea6bd40b319d815411a12577b230eee592854b
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-07-19 17:38:38 +0000

    Sample routes update

--- generators/lwt_authentication_system/templates/config/routes.rb
+++ generators/lwt_authentication_system/templates/config/routes.rb
@@ -1,7 +1,8 @@
 map.with_options :controller => "users" do |users_map|
-  users_map.login "login", :action => "login"
-  users_map.logout "logout", :action => "logout"
-  users_map.reminder "reminder", :action => "reminder"
-  users_map.profile "profile", :action => "profile"
-  users_map.signup "signup", :action => "signup"
+  users_map.login "users/login", :action => "login"
+  users_map.reminder_login "users/:id/login/:token", :action => "login"
+  users_map.logout "users/logout", :action => "logout"
+  users_map.reminder "users/reminder", :action => "reminder"
+  users_map.profile "users/profile", :action => "profile"
+  users_map.signup "users/signup", :action => "signup"
 end
