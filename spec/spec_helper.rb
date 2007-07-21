80eb66293be30d4041757181e17cd9976f857aed
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-07-21 05:33:37 +0000

    Force schema reload. work out file path issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'fileutils'
 
+plugin_spec_directory = File.expand_path(File.dirname(__FILE__))
+
 # Lets load up a rails environment
 unless defined?(RAILS_ROOT)
- RAILS_ROOT = ENV["RAILS_ROOT"] || File.expand_path(File.join(File.dirname(__FILE__), "../../../.."))
+ RAILS_ROOT = ENV["RAILS_ROOT"] || File.join(plugin_spec_directory, "../../../..")
 end
 require File.join(RAILS_ROOT, "spec", "spec_helper")
 # require File.join(File.dirname(__FILE__), "..", "init")
@@ -13,27 +15,27 @@ ActionController::Routing::Routes.draw {|m| m.connect ':controller/:action/:id' 
 ActionController::Routing.use_controllers! %w(users account admin/users)
 
 # Setup logging and db connection
-FileUtils.mkdir( File.join( File.dirname(__FILE__), 'tmp' ) )
-ActiveRecord::Base.logger = Logger.new File.join( File.dirname(__FILE__), 'tmp/test.log' )
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => File.join( File.dirname(__FILE__), 'tmp/test.db' )
+FileUtils.mkdir( File.join( plugin_spec_directory, 'tmp' ) ) unless File.exists?( File.join( plugin_spec_directory, 'tmp' ) )
+ActiveRecord::Base.logger = Logger.new File.join( plugin_spec_directory, 'tmp/test.log' )
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => File.join( plugin_spec_directory, 'tmp/test.db' )
 
 # Load schema
 ActiveRecord::Schema.suppress_messages do
   ActiveRecord::Schema.define do
-    create_table :groups do |t|
+    create_table :groups, :force => true do |t|
       t.column :name, :string
     end
 
-    create_table :privileges do |t|
+    create_table :privileges, :force => true do |t|
       t.column :name, :string
     end
 
-    create_table :groups_privileges do |t|
+    create_table :groups_privileges, :force => true do |t|
       t.column :group_id, :integer
       t.column :privilege_id, :integer
     end
 
-    create_table :users do |t|
+    create_table :users, :force => true do |t|
       t.column :username, :string
       t.column :password_hash, :string
       t.column :group_id, :integer
@@ -41,13 +43,18 @@ ActiveRecord::Schema.suppress_messages do
       t.column :active, :boolean
     end
   
-    create_table :user_reminders do |t|
+    create_table :user_reminders, :force => true do |t|
       t.column :user_id, :integer
       t.column :token, :string
       t.column :expires_at, :datetime
     end
   end
 end
+
+# ActiveRecord models
+class DefaultActiveRecordModel < ActiveRecord::Base
+end
+
 
 # class UsersController < ActionController::Base
 # end
