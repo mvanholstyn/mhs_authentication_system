c35b60e29004b3a0987bec46fa774dbc6d940da4
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-07-21 05:24:04 +0000

    Move config to spec_helper... update spec_helper to work more

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,25 @@
+require 'fileutils'
+
+# Lets load up a rails environment
 unless defined?(RAILS_ROOT)
  RAILS_ROOT = ENV["RAILS_ROOT"] || File.expand_path(File.join(File.dirname(__FILE__), "../../../.."))
 end
 require File.join(RAILS_ROOT, "spec", "spec_helper")
 # require File.join(File.dirname(__FILE__), "..", "init")
 
+# Setup up routes for our tests
 ActionController::Routing::Routes.clear!
 ActionController::Routing::Routes.draw {|m| m.connect ':controller/:action/:id' }
 ActionController::Routing.use_controllers! %w(users account admin/users)
 
-ActiveRecord::Base.logger = Logger.new File.join( dir, 'log/test.log' )
-ActiveRecord::Base.establish_connection YAML.load_file( File.join( dir, 'config/database.yml' ) )[:test]
+# Setup logging and db connection
+FileUtils.mkdir( File.join( File.dirname(__FILE__), 'tmp' ) )
+ActiveRecord::Base.logger = Logger.new File.join( File.dirname(__FILE__), 'tmp/test.log' )
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => File.join( File.dirname(__FILE__), 'tmp/test.db' )
 
+# Load schema
 ActiveRecord::Schema.suppress_messages do
-  require File.join( dir, 'db/schema' )
+  require File.join( File.dirname(__FILE__), 'db/schema' )
 end
 
 # class UsersController < ActionController::Base
