90e4d86cd90e1bdebe4bdaac31784e9542c81e38
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-07-21 05:25:12 +0000

    more shifting... of config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,34 @@ ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => File
 
 # Load schema
 ActiveRecord::Schema.suppress_messages do
-  require File.join( File.dirname(__FILE__), 'db/schema' )
+  ActiveRecord::Schema.define do
+    create_table :groups do |t|
+      t.column :name, :string
+    end
+
+    create_table :privileges do |t|
+      t.column :name, :string
+    end
+
+    create_table :groups_privileges do |t|
+      t.column :group_id, :integer
+      t.column :privilege_id, :integer
+    end
+
+    create_table :users do |t|
+      t.column :username, :string
+      t.column :password_hash, :string
+      t.column :group_id, :integer
+      t.column :email_address, :string
+      t.column :active, :boolean
+    end
+  
+    create_table :user_reminders do |t|
+      t.column :user_id, :integer
+      t.column :token, :string
+      t.column :expires_at, :datetime
+    end
+  end
 end
 
 # class UsersController < ActionController::Base
